37dfdbfdfa9734e727c8364256438cee21171530
Cosmin873 <cosmin.and117@gmail.com>
2026-01-12 16:02:25 +0200

    Add api key

--- The Complete JavaScript Course - JONAS SCHMEDTMANN (UDEMY)/Forkify/src/js/config.js
+++ The Complete JavaScript Course - JONAS SCHMEDTMANN (UDEMY)/Forkify/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '03131ae1-3041-42fc-8144-0d89e650896e';
 export const MODAL_CLOSE_SEC = 2.5;
